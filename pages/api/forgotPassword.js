dec1d2b5c2933097b69247b56b52a76338c41722
ashdephoenix123 <akashsarki12345@gmail.com>
2025-10-16 21:27:52 +0530

    test5

--- pages/api/forgotPassword.js
+++ pages/api/forgotPassword.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
           console.error("Error sending email:", err.body);
         });
 
-      res.status(200).json({ success: true });
+      res.status(200).json({ success: true, url, findUser, sender });
     } else {
       throw new Error("Request method not allowed");
     }
