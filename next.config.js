f67c9f6e315e412b5a0def851d885800741b8558
ashdephoenix123 <akashsarki12345@gmail.com>
2025-10-20 10:19:32 +0530

    updated next config file

--- next.config.js
+++ next.config.js
@@ -21,6 +21,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3001",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
