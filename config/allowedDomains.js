ef081bde227f49a7d96136e008ac56fb86922aba
ashdephoenix123 <akashsarki12345@gmail.com>
2025-11-02 21:14:07 +0530

    added domain in allow list

--- config/allowedDomains.js
+++ config/allowedDomains.js
@@ -2,5 +2,6 @@ export const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://nextecomm.netlify.app/",
+  "https://next-ecomm-product-management.vercel.app/",
 ];
 // add production domain of product management
